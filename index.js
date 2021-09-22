4c9269d44db30c29a2ec87c1ef1de1d14edcae71
Moon-2006 <90346045+Moon-2006@users.noreply.github.com>
2021-09-22 09:41:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-//Variables
+//Variables and setup
 var buttonToggle = true;
 var checkboxToggle = false;
 var newText = document.getElementById("textInput").value;
-var originTime = new Date().getTime();
-var loop = setInterval(function(){
-    var now = new Date().getTime();
-    var timeLeft = originTime-now;
-    var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000) + 12;
-    if(seconds >= 0){
-        document.getElementById("timer").innerHTML = seconds;
-    }
-}, 1000)
+var diffuculty = 4;
+var numString = [];
+
+showTextBox();
+
+//diffuculty += 1
+
 
 
 //console
@@ -19,16 +17,73 @@ console.log(buttonToggle);
 
 
 //Functions 
+
+function showTextBox() {
+    var x = document.getElementById("textBox");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function showOnStart() {
+    var x = document.getElementById("showOnStart");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+//onClick
 function onClick () {
+    //generate random numbers
+    for (var i = 0; i < diffuculty; i++){
+        var randomNum = getRand();
+        document.getElementById("numberSlot").innerHTML += " " + randomNum;
+        numString += randomNum;
+    }    
+
+    //timer
+    var originTime = new Date().getTime();
+
+    var loop = setInterval(function(){
+        var now = new Date().getTime();
+        var timeLeft = originTime-now;
+        var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000) + 7;
+        checkHide();
+        if(seconds >= 0){
+            document.getElementById("timer").innerHTML = seconds;
+        }
+    }, 1000);
+
     console.log(Timer.getTime());
+    showOnStart();
 }
 
+
+//Checkbox
 function toggle () {
+    checkHide();
     console.log(checkbox.checked);
 }
 
+//Change text
 function submitText () {
     newText = document.getElementById("textInput").value;
     document.getElementById("paragraph").innerHTML = newText;
     console.log("New text set to: ",newText);
+}
+
+//Add random number
+function getRand () {
+    var getRandNum = Math.floor(Math.random()*10);
+    return(getRandNum);
+}
+
+//Hide string of numbers
+function hideNums() {
+    document.getElementById("numberSlot").innerHTML = ("Times up! enter the numbers you just saw in the box below")
+    showTextBox();
 }
